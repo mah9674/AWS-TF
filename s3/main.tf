40732810ae6e62bfde917429323caf3dc94a4127
Mahasweta Bhattacharjee <bhattacharyamahasweta09@gmail.com>
2026-02-18 20:28:09 +0530

    add cloudfront to s3

--- s3/main.tf
+++ s3/main.tf
@@ -43,3 +43,45 @@ resource "aws_s3_object" "index" {
   etag   = filemd5("index.html")
   content_type = "text/html"
 }
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.demo.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 CloudFront CDN"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
