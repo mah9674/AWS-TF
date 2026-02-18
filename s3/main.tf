bb59239dc4121d417ae9c21bd5b0d20aa8530edf
Mahasweta Bhattacharjee <bhattacharyamahasweta09@gmail.com>
2026-02-18 20:59:26 +0530

    bucket policy modification

--- s3/main.tf
+++ s3/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# ---------------- S3 bucket ----------------
 resource "aws_s3_bucket" "demo" {
   bucket = "mah-demo-bucket-130282"
 
@@ -14,21 +15,39 @@ resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.demo.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
+# Bucket policy to allow public read
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.demo.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.demo.arn}/*"
+      }
+    ]
+  })
+}
+
+# ---------------- Second bucket ----------------
 resource "aws_s3_bucket" "demo1" {
   bucket = "mah-demo1-bucket-200488"
 
   tags = {
-    Name = "mah-demo-bucket"
+    Name = "mah-demo1-bucket"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "block1" {
-  bucket = aws_s3_bucket.demo.id
+  bucket = aws_s3_bucket.demo1.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -36,14 +55,16 @@ resource "aws_s3_bucket_public_access_block" "block1" {
   restrict_public_buckets = true
 }
 
+# ---------------- index.html upload ----------------
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.demo.id
-  key    = "index.html"
-  source = "index.html"
-  etag   = filemd5("index.html")
+  bucket       = aws_s3_bucket.demo.id
+  key          = "index.html"
+  source       = "index.html"
+  etag         = filemd5("index.html")
   content_type = "text/html"
 }
 
+# ---------------- CloudFront ----------------
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = aws_s3_bucket.demo.bucket_regional_domain_name
@@ -68,7 +89,6 @@ resource "aws_cloudfront_distribution" "cdn" {
 
     forwarded_values {
       query_string = false
-
       cookies {
         forward = "none"
       }
@@ -85,3 +105,4 @@ resource "aws_cloudfront_distribution" "cdn" {
     cloudfront_default_certificate = true
   }
 }
+
